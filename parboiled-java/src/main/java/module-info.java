c1340ef5b0f7b77ee63370a42b2cce411cd396b7
imagingbook <wilbur@ieee.org>
2023-01-25 14:07:11 +0100

    JavaDoc setup for modules (rebuild), POM setup for deploy

--- parboiled-java/src/main/java/module-info.java
+++ parboiled-java/src/main/java/module-info.java
@@ -1,3 +1,10 @@
+/**
+ * This is a modularized version of the <strong>parboiled</strong> PEG parser Java/Scala implementation originally
+ * developed by Mathias Doenitz, refactored from
+ * <a href= https://github.com/sirthias/parboiled/tree/6c9cded0ea240e572903234384d759562669877c>Version 1.4.1</a>
+ * (Jan. 2023). See <a href="https://github.com/sirthias/parboiled">https://github.com/sirthias/parboiled</a> for the
+ * original project.
+ */
 module imagingbook.parboiled.java {
     requires org.objectweb.asm;
     requires org.objectweb.asm.tree;
